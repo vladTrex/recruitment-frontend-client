4fee49b980bbb9e6e6a1248b2bc028385a572656
Vladyslav Tertyshnyi <vladislafus@gmail.com>
2018-03-08 20:08:48 +0200

    Add hash router + 404 page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './containers/homeContainer';
+
+export const Whoops404 = ({ location }) =>
+    <div className='whoops-404'>
+        <h2>Resource not found at '{location.pathname}'</h2>
+    </div>
 
 window.React = React;
 
 render(
-    <Home />,
+    <HashRouter>
+        <div className='main'>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route component={Whoops404} />
+            </Switch>
+        </div>
+    </HashRouter>
+    ,
     document.getElementById('rootApp')
 );
