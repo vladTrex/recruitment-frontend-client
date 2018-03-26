9dcca0938a4da35a1bbe079ad69133237a1908bb
Vladyslav Tertyshnyi <vladislafus@gmail.com>
2018-03-26 20:31:09 +0300

    Change pages

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => 
     <div>
-        <h4>Menu</h4>
+        <Link to='/'>Home</Link>
     </div>
--- src/components/vacancy.js
+++ src/components/vacancy.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import Menu from './menu';
+
 export default ({ vacancy }) => 
     <div>
+        <Menu />
         <h4>{vacancy.title}</h4>
         <p>Recruitment App.</p>
     </div>
