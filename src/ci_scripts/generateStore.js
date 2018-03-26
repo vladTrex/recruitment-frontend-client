19ba6f69e8d0cf309eace28fb4a26c55c4791510
Vladyslav Tertyshnyi <vladislafus@gmail.com>
2018-03-26 21:10:49 +0300

    Create script for generating store

---
+++ src/ci_scripts/generateStore.js
@@ -0,0 +1,27 @@
+const uuidv4 = require('uuid/v4');
+const fs = require('fs');
+
+const source = [
+    {title: 'Vacancy 1', description: 'Some description for vacancy 1'},
+    {title: 'Vacancy 2', description: 'Some description for vacancy 2'},
+    {title: 'Vacancy 3', description: 'Some description for vacancy 3'},
+    {title: 'Vacancy 4', description: 'Some description for vacancy 4'},
+];
+
+console.log('-- Start generate store --');
+console.log(uuidv4());
+
+const vacancies = source.map(item => {
+    const newItem = Object.assign({}, item, {id: uuidv4()});
+    console.log(newItem);
+    return newItem;
+});
+
+const store = {vacancies: vacancies};
+
+fs.writeFile('./src/store/store.json', JSON.stringify(store, null, 4), 'utf8', function (err) {
+    if (err) throw err;
+    console.log('-- Store was generated --');
+});
+  
+
