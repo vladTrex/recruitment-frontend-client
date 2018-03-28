ae6cae97081c4bfbc950b7ebeba76b21899ef644
Vladyslav Tertyshnyi <vladislafus@gmail.com>
2018-03-28 22:51:09 +0300

    Add correct test for get vacancy by id

--- src/models/__tests__/vacancy.js
+++ src/models/__tests__/vacancy.js
@@ -2,6 +2,13 @@ import { getVacancyById } from '../vacancy';
 
 describe('getVacancyById', () => {
     it('should return found vacancy', () => {
-        
+        const vacancies = [
+            {id: '123'},
+            {id: '234'}
+        ];
+        const vacancyId = '123';
+        const expected = {id: '123'};
+
+        expect(getVacancyById(vacancies)(vacancyId)).toEqual(expected);
     });
 });
